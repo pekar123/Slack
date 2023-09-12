2e42690e83cdc324427baf373b9ba18cc628f209
pekar123 <makakakill@gmail.com>
2023-09-13 02:35:40 +0300

    homework

--- src/route/index.js
+++ src/route/index.js
@@ -12,7 +12,10 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+
+    
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
