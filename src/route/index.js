5dd1939ef988ab9b836a8a3f48e30419782aaa3c
pekar123 <makakakill@gmail.com>
2023-11-03 16:04:34 +0200

    dashboard

--- src/route/index.js
+++ src/route/index.js
@@ -28,6 +28,16 @@ router.get('/slack', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+router.get('/dashboard', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('dashboard', {
+    layout: 'dashboard',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 // ================================================================
 
 // Підключаємо роутер до бек-енду
